885cfad2d0a45fc40173b1609c0c6a428eff07b5
Aaron Camacho <CamachoAaron7@gmail.com>
2019-08-29 20:05:17 -0600

    Update geocode-locations.py

--- geocode-locations.py
+++ geocode-locations.py
@@ -1,16 +1,16 @@
 from geopy import geocoders
 import csv
-g_api_key = 'AIzaSyCJhIh2wAd05KoUkGtXsJwYynhuJ-Ms1Ak'
+g_api_key = 'your_Google_API'
 g = geocoders.GoogleV3(g_api_key)
 
-filename = "costcos-geocoded.csv" 
-f = open(filename, "w") 
-headers = "geo-codes\n" 
+filename = "costcos-geocoded.csv"
+f = open(filename, "w")
+headers = "geo-codes\n"
 f.write(headers)
 
 costcos = csv.reader(open('costcos-limited.csv'), delimiter=',')
 next(costcos) # Skip header
-for row in costcos: 
+for row in costcos:
 	full_addy = row[1] + "," + row[2] + "," + row[3] + "," + row[4]
 	try:
 		place, (lat, lng) = list(g.geocode(full_addy, exactly_one=False))[0]
